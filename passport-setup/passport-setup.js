74433985c5ae5e494452fb23d6d4404a3f606c08
rehmanali17 <rehmanmalik17@gmail.com>
2021-08-20 08:19:28 +0500

    Google Profile Picture

--- passport-setup/passport-setup.js
+++ passport-setup/passport-setup.js
@@ -28,18 +28,20 @@ passport.use(new GoogleStartegy({
 },(accessToken,refreshToken,profile,done)=>{
     const name = profile.displayName
     const email = profile._json.email
+    const image_url = profile._json.picture
     var sql = "SELECT * from users where u_email=?";
         conn.query(sql,[email], async (err,result)=>{
             if(!err){
                 const user = {
                     u_email:email,
-                    u_name: name
+                    u_name: name,
+                    image_url: image_url,
                 }
                 if(result.length > 0){
                     done(null,result[0])
                 }else{
-                    var sql = "INSERT into users(u_email,u_name) values(?,?)";
-                    let query = mysql.format(sql,[email,name]);
+                    var sql = "INSERT into users(u_email,u_name,image_url) values(?,?,?)";
+                    let query = mysql.format(sql,[email,name,image_url]);
                     conn.query(query,(err,result)=>{
                         if(!err){
                             if(result.affectedRows == 1){
